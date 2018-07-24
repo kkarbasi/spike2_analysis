3e75794ad038adfe386f6d86a1a7646c1cd86478
Kaveh Karbasi <kkarbasi@berkeley.edu>
2018-07-24 19:30:16 -0400

    added spike aligning and spike averaging

--- kaveh/sorting/spikesorter.py
+++ kaveh/sorting/spikesorter.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2018 Laboratory for Computational Motor Control
+Copyright (c) 2018 Laboratory for Computational Motor Control, Johns Hopkins School of Medicine
 
 Author: Kaveh Karbasi <kkarbasi@berkeley.edu>
 """
@@ -7,6 +7,8 @@ Author: Kaveh Karbasi <kkarbasi@berkeley.edu>
 import numpy as np
 from sklearn.mixture import GaussianMixture
 import scipy.signal
+from kaveh.plots import axvlines
+from matplotlib import pyplot as plt
 
 class SimpleSpikeSorter:
     """ Class that detects and sorts simple spikes"""
@@ -27,7 +29,7 @@ class SimpleSpikeSorter:
     def run(self):
         self._pre_process()
         self._detect_spikes()
-        
+        self._align_spikes()        
 
     def _pre_process(self):
         """
@@ -58,20 +60,54 @@ class SimpleSpikeSorter:
         peak_times,_ = scipy.signal.find_peaks(self.voltage[all_spike_indices])
         self.spike_indices = all_spike_indices[peak_times]
 
+    def _align_spikes(self):
+        """
+        Aligns the spike waveforms (from pre_window to post_windows, aligned to peak times)
+        """
+        pre_index = int(np.round(self.pre_window / self.dt))
+        post_index = int(np.round(self.post_window / self.dt))
+
+        self.aligned_spikes = np.array([self.voltage[i - pre_index : i + post_index ] for i in self.spike_indices]) 
+
+    # TODO
+    def _choose_num_features(self, captured_variance=0.75):
+        """
+        Use the number of components that captures at least captured_variance of the spike waveforms
+        """
+        return 0
+
+
+    def set_spike_window(self, pre_time, post_time):
+        self.pre_window = pre_time
+        self.post_window = post_time
+
+    def plot_spike_waveforms_average(self, **kw):
+        """
+        Plots the average spike wavelets of the current dataset
+        """
+        spikes_avg = np.mean(self.aligned_spikes, axis = 0)
+        spikes_std = np.std(self.aligned_spikes, axis = 0)
+        x = np.arange(0,self.aligned_spikes.shape[1])
+
+        plt.figure()
+        l = plt.plot(x, spikes_avg, **kw)
+
+        plt.fill_between(x, spikes_avg - spikes_std, spikes_avg + spikes_std, color=l[0].get_color(), alpha=0.25)
+        
+
     def plot_spike_peaks(self, figsize=(21,5)):
         """
         Plots the voltage signal, overlaid by spike peak times,
         overlaid by lines indicating the window around the spike peaks
         """
         
-        from kaveh.plots import axvlines
-        from matplotlib import pyplot as plt
 
         plt.figure(figsize = figsize)
         plt.plot(self.voltage)
         plt.plot(self.spike_indices, self.voltage[self.spike_indices], '.r')
-        axvlines(plt.gca(), self.spike_indices + self.post_window/self.dt, color='g')
-        axvlines(plt.gca(), self.spike_indices - self.pre_window/self.dt, color='m')
+        axvlines(plt.gca(), self.spike_indices + int(np.round(self.post_window/self.dt)), color='g')
+        axvlines(plt.gca(), self.spike_indices - int(np.round(self.pre_window/self.dt)), color='m')
+
 
 
 
