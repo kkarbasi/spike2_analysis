198d68d0ec2471a2c85c8c5db1869f5be881a5cb
Kaveh Karbasi <kkarbasi@berkeley.edu>
2018-12-03 12:02:47 -0500

    updated the oculomotor target class to detect and cluster jump vectors using kmeans

--- kaveh/behavioral/oculomotor.py
+++ kaveh/behavioral/oculomotor.py
@@ -1,17 +1,19 @@
 """
-Copyright (c) 2018 Laboratory for Computational Motor Control, Johns Hopkins School of Medicine
+Laboratory for Computational Motor Control, Johns Hopkins School of Medicine
 
 Author: Kaveh Karbasi <kkarbasi@berkeley.edu>
 """
 
 import numpy as np
 import scipy.signal
+from sklearn.cluster import KMeans
 
 class target:
     """ Class for handling target data"""
     def __init__(self, vt, ht, dt, mode):
         """
         Object constructor
+        m sklearn.cluster import KMeans
         vt: vertical target position signal
         ht: horizontal target position signal
         dt: sampling period
@@ -65,40 +67,68 @@ class target:
     def _find_target_jumps_2d(self):
         return 0
 
-    def _find_jump_vector_amplitudes(self, bin_size):
+    def _find_jump_vector_amplitudes(self, num_clusters):
         if self.mode == 'horizontal':
-            return self._find_jump_vector_amplitudes_h( bin_size)
+            return self._find_jump_vector_amplitudes_h(num_clusters)
         if self.mode == 'vertical':
-            return self._find_jump_vector_amplitudes_v( bin_size)
+            return self._find_jump_vector_amplitudes_v(num_clusters)
         if self.mode == '2d':
-            return self._find_jump_vector_amplitudes_2d( bin_size)
+            return self._find_jump_vector_amplitudes_2d(num_clusters)
 
-    def _find_jump_vector_amplitudes_h(self, bin_size):
+    def _find_jump_vector_amplitudes_h(self, num_clusters):
         target_jump_indices = self._find_target_jumps()
-        jump_vecs = []
+        self.jump_vecs = []
         for tji in target_jump_indices:
-                jump_vecs = jump_vecs + [self.ht[tji + 5] - self.ht[tji - 5]]
-        [hist, bin_edges] = np.histogram(jump_vecs, bins=np.arange(np.min(self.ht), np.max(self.ht), bin_size))
-        hist[hist < 10] = 0 # remove rare target jump vectors
-        return bin_edges[np.nonzero(hist)]
+                self.jump_vecs = self.jump_vecs + [self.ht[tji + 5] - self.ht[tji - 5]]
+        #[hist, bin_edges] = np.histogram(jump_vecs, bins=np.arange(np.min(self.ht), np.max(self.ht), bin_size))
+        #hist[hist < 10] = 0 # remove rare target jump vectors
+        #return bin_edges[np.nonzero(hist)]
+        self.jump_vecs = np.array(self.jump_vecs).reshape(-1,1)
+        kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(self.jump_vecs)
+        jump_amps = kmeans.cluster_centers_
+        jump_amps = np.array([int(ja) for ja in jump_amps])
+        return jump_amps
+    
+    def _is_in_cluster(self, jump_vec, jump_amp, jump_tol):
+        if self.mode == 'horizontal':
+            if jump_vec < jump_amp + jump_tol and jump_vec >= jump_amp - jump_tol:
+                return True
+            else:
+                return False
+        if self.mode == 'vertical':
+            # TODO
+            return False
+        if self.mode == '2d':
+            # TODO
+            return False
 
-    def get_target_jumps(self, bin_size = 200):
+    def get_target_jumps(self, num_clusters = 3, jump_tol = 100):
         """
         Returns a dictionary containing the indices of the jumps to the found jump vectors.
-        The jump vectors are found by detecting all the jumps, and finding the histogram of
-        the amplitudes (in vertical and horizontal modes only for now). The bin size basically
-        determines the resolution of detecting amplitudes
+        The jump vectors are found by detecting all jumps, then using kmeans with k=num_clusters
+        (should be determined by the experimental setup, auto detection later), then assigning each
+        jump to one cluster if the euclidean distance is less than jump_tol.
         """
-        jump_amps = self._find_jump_vector_amplitudes(bin_size)
+        jump_amps = self._find_jump_vector_amplitudes(num_clusters)
+
         target_jumps_to = {}
         for ja in jump_amps:
-                target_jumps_to[ja] = np.array([])
+                target_jumps_to[ja] = np.array([], dtype='int64')
         target_jump_indices = self._find_target_jumps()
-        for tji in target_jump_indices:
-                jump_vec = self.ht[tji + 5] - self.ht[tji - 5]
+        for i, tji in enumerate(target_jump_indices):
+                #     jump_vec = ht.data[prange][tji + 5] - ht.data[prange][tji - 5]
                 for ja in jump_amps:
-                    if jump_vec < ja + bin_size and jump_vec >= ja:
+                    if self._is_in_cluster(self.jump_vecs[i], ja, jump_tol):
                         target_jumps_to[ja] = np.concatenate((target_jumps_to[ja], [tji]))
+        #target_jumps_to = {}
+        #for ja in jump_amps:
+        #        target_jumps_to[ja] = np.array([])
+        #target_jump_indices = self._find_target_jumps()
+        #for tji in target_jump_indices:
+        #        jump_vec = self.ht[tji + 5] - self.ht[tji - 5]
+        #        for ja in jump_amps:
+        #            if jump_vec < ja + bin_size and jump_vec >= ja:
+        #                target_jumps_to[ja] = np.concatenate((target_jumps_to[ja], [tji]))
         return target_jumps_to
 
 
